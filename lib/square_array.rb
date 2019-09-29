5641b832f229e09702c7cd08bf51de977b646922
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-29 23:31:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,16 @@
 def square_array(array)
+<<<<<<< HEAD
   numbers = []
   array { |i| numbers << i ** 2 }
   numbers
 end
 my_numbers = [1,2,3]
 square_array(my_numbers)
+=======
+  numbers = [1, 2, 3]
+  square_array(numbers)
+  new_numbers = [9, 10, 16, 25]
+  square_array(new_numbers)
+
+end
+>>>>>>> 7c83a12a842edb97345dcb5395e2e2bdfd2825c8
